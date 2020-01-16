a0a4edd6b2516da1e2f096b7d1ba96006bb9bde9
Irene Font Peradejordi <if76@cornell.edu>
2020-01-16 15:11:00 +0100

    add files

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,318 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/1 Correct Answer</name>
+        <message>
+            <source>Well done! I was indeed an elephant. You are super smart! Ok, I will play another animal now. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Well done! I was indeed an elephant. You are super smart! Ok, I will play another animal now. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/1 Wrong Answer</name>
+        <message>
+            <source>Nice try! Unfortunately the answer is not correct. I was an elephant. Let's have another try!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Nice try! Unfortunately the answer is not correct. I was an elephant. Let's have another try!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/2 Correct Answer </name>
+        <message>
+            <source>Wow, you guessed it right! I was indeed a mouse. Let's do a last round. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wow, you guessed it right! I was indeed a mouse. Let's do a last round. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/2 Wrong Answer </name>
+        <message>
+            <source>Ah you were almost there. Unfortunately the answer is not correct. I was a mouse. Let's play the last round. I am pretty sure you willl guess this one right. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ah you were almost there. Unfortunately the answer is not correct. I was a mouse. Let's play the last round. I am pretty sure you willl guess this one right. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/3 Correct Answer </name>
+        <message>
+            <source>Well done! You guessed the last question right! You are a star!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Well done! You guessed the last question right! You are a star!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/3 Wrong Answer  </name>
+        <message>
+            <source>Ah too bad! The answer is not correct unfortunately. I was actually mimicking a gorilla. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ah too bad! The answer is not correct unfortunately. I was actually mimicking a gorilla. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Let's dance together!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Let's dance together!</translation>
+        </message>
+        <message>
+            <source>Awesome, let's go!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Awesome, let's go!</translation>
+        </message>
+        <message>
+            <source>Sorry, I did not understand. Please repeat</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Sorry, I did not understand. Please repeat</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Let's dance together!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Let's dance together!</translation>
+        </message>
+        <message>
+            <source>Awesome, let's go!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Awesome, let's go!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Sweet! Before we start, I want to share the rules of the game. I will mimick some animals and then you have to guess which animal I am. Ok, I am going to start with the first animal now! </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Sweet! Before we start, I want to share the rules of the game. I will mimick some animals and then you have to guess which animal I am. Ok, I am going to start with the first animal now! </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>The game is over. Many thanks for participating.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">The game is over. Many thanks for participating.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Bye - AS</name>
+        <message>
+            <source>All good. Have a good day! Bye.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">All good. Have a good day! Bye.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice/Dance - AS</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You want to dance? Splendid! </source>
+            <comment>Text</comment>
+            <translation type="unfinished">You want to dance? Splendid! </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice/Game - AS</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Oh you want to play a game? Sweet!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Oh you want to play a game? Sweet!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice/Holiday - AS</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>So you want to talk about holiday destinations? Cool!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">So you want to talk about holiday destinations? Cool!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/DANCE/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Awesome, let's go!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Awesome, let's go!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/DANCE/Animated Say (1)</name>
+        <message>
+            <source>Awesome, let's go!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Awesome, let's go!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Awesome, let's move this awesome body!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Awesome, let's move this awesome body!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/DANCE/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>YEAH! You rock! It was a pleasure to share this dance with you!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">YEAH! You rock! It was a pleasure to share this dance with you!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/DANCE/Animated Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alright!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alright!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/GAME/1 Correct Answer</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Well done! I was indeed an elephant. You are super smart! Ok, I will play another animal now. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Well done! I was indeed an elephant. You are super smart! Ok, I will play another animal now. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/GAME/1 Wrong Answer</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Nice try! Unfortunately the answer is not correct. I was an elephant. Let's have another try!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Nice try! Unfortunately the answer is not correct. I was an elephant. Let's have another try!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/GAME/2 Correct Answer </name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Wow, you guessed it right! I was indeed a mouse. Let's do a last round. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Wow, you guessed it right! I was indeed a mouse. Let's do a last round. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/GAME/2 Wrong Answer </name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ah you were almost there. Unfortunately the answer is not correct. I was a mouse. Let's play the last round. I am pretty sure you willl guess this one right. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ah you were almost there. Unfortunately the answer is not correct. I was a mouse. Let's play the last round. I am pretty sure you willl guess this one right. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/GAME/3 Correct Answer </name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Well done! You guessed the last question right! You are a star!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Well done! You guessed the last question right! You are a star!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/GAME/3 Wrong Answer  </name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ah too bad! The answer is not correct unfortunately. I was actually mimicking a gorilla. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ah too bad! The answer is not correct unfortunately. I was actually mimicking a gorilla. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/GAME/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Sweet! Before we start, I want to share the rules of the game. I will mimick some animals and then you have to guess which animal I am. Ok, I am going to start with the first animal now! </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Sweet! Before we start, I want to share the rules of the game. I will mimick some animals and then you have to guess which animal I am. Ok, I am going to start with the first animal now! </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/GAME/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>The game is over. Many thanks for participating.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">The game is over. Many thanks for participating.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/GAME/Other</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Too bad! Wish you a nice day though!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Too bad! Wish you a nice day though!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Other</name>
+        <message>
+            <source>Too bad! Wish you a nice day though!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Too bad! Wish you a nice day though!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Survey - AS</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Sweet. Thank you so much! I will show the survey on the tablet now. Once you are done, please touch my head!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Sweet. Thank you so much! I will show the survey on the tablet now. Once you are done, please touch my head!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Thanks - AS</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Many thanks for filling out the survey! Have a blissful day! Bye. </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Many thanks for filling out the survey! Have a blissful day! Bye. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Timeline_Dance/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Dance with me</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dance with me</translation>
+        </message>
+    </context>
+</TS>
